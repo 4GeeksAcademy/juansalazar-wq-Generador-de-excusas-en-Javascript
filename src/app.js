6990fccad787d9caf2b61a71cf86f8cd4d933a94
Juan Salazar <juansalazar@agenciajamarketingdigital.com>
2026-01-22 17:21:56 +0000

    add OMG! you will not believe me but...

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
 
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
